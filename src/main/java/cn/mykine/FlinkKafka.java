0feee9e7c37a9e345b42a42ab8d2280ece7445dc
jy@mykine <mykine@sina.com>
2023-02-27 15:45:38 +0800

    flink从Kafka数据源读取数据

---
+++ src/main/java/cn/mykine/FlinkKafka.java
@@ -0,0 +1,64 @@
+package cn.mykine;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.Kafka011TableSource;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
+
+public class FlinkKafka {
+    public static void main(String[] args) throws Exception {
+        //env-创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //source-从数据源获取流数据
+        // kafka 配置项
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.10.135:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStreamSource<String> receiveStreamData
+                = env.addSource(new FlinkKafkaConsumer011<String>(
+                                        "sensor",
+                                        new SimpleStringSchema(),
+                                        properties
+                                    )
+                                );
+
+
+        //transformation-基于流数据进行转换计算
+        SingleOutputStreamOperator<Tuple2<String, String>> result = receiveStreamData
+                .flatMap(new MyFlatMap())
+                .setParallelism(2);
+
+        //sink-输出结果
+        result.print();
+
+        //执行
+        env.execute();
+
+    }
+
+    public static class MyFlatMap implements FlatMapFunction<String, Tuple2<String,String>>{
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, String>> out) throws Exception {
+            String[] words = value.split(" ");
+
+            out.collect(new Tuple2<>(words[0],words[2]));
+        }
+    }
+}
