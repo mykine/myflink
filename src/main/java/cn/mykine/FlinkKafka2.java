fe87cc580ee47ed82277cfaeb132c9986c92187b
jy@mykine <mykine@sina.com>
2023-03-01 17:32:25 +0800

    flink-合流操作-union多个相同数据结构流数据

---
+++ src/main/java/cn/mykine/FlinkKafka2.java
@@ -0,0 +1,106 @@
+package cn.mykine;
+
+import cn.mykine.apitest.bean.SensorReading;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.util.Collector;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class FlinkKafka2 {
+
+    public static void main(String[] args) throws Exception {
+        //env-创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //source-从数据源获取流数据
+        // kafka 配置项
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.10.135:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStreamSource<String> receiveStreamData
+                = env.addSource(new FlinkKafkaConsumer011<String>(
+                                        "sensor",
+                                        new SimpleStringSchema(),
+                                        properties
+                                    )
+                                );
+
+
+        //transformation-基于流数据进行转换计算
+
+
+
+
+        DataStream<SensorReading> dataStream = receiveStreamData.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String value) throws Exception {
+                String[] valueArr = value.split(",");
+                return new SensorReading(valueArr[0],
+                        Long.valueOf(valueArr[1]),
+                        Double.valueOf(valueArr[2]));
+            }
+        });
+
+
+        //分流操作split，给流数据打标签
+        SplitStream<SensorReading> splitStream =
+                dataStream.split(new OutputSelector<SensorReading>() {
+            @Override
+            public Iterable<String> select(SensorReading value) {
+                return value.getTemperature() > 30 ? Collections.singletonList("high") : Collections.singletonList("low");
+            }
+        });
+
+        DataStream<SensorReading> highData = splitStream.select("high");
+        DataStream<SensorReading> lowData = splitStream.select("low");
+        DataStream<SensorReading> allData = splitStream.select("low","high");
+
+        //union操作合并多条相同数据结构的流
+        DataStream<SensorReading> unionData = highData.union(lowData, allData);
+
+        //sink-输出结果
+        unionData.print("union数据");
+
+        //执行
+        env.execute();
+
+    }
+
+    public static class MyMap implements MapFunction<String,String>{
+
+        @Override
+        public String map(String value) throws Exception {
+            String[] words = value.split(",");
+            return words[0]+"的温度是"+words[2];
+        }
+    }
+
+    public static class MyFlatMap implements FlatMapFunction<String, Tuple2<String,String>>{
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, String>> out) throws Exception {
+            String[] words = value.split(",");
+
+            out.collect(new Tuple2<>(words[0],words[2]));
+        }
+    }
+}
