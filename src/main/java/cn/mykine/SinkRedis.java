38bf47672e5786c732df074b2b0eb8e86b363e9c
jy@mykine <mykine@sina.com>
2023-03-02 16:25:54 +0800

    flink-sink到单机Redis

---
+++ src/main/java/cn/mykine/SinkRedis.java
@@ -0,0 +1,108 @@
+package cn.mykine;
+
+import cn.mykine.apitest.bean.SensorReading;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+import java.util.Properties;
+
+public class SinkRedis {
+
+    public static void main(String[] args) throws Exception {
+        //env-创建执行环境
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment()
+                        .setParallelism(3);
+
+        //source-从数据源获取流数据
+        // kafka 配置项
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.10.135:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStreamSource<String> receiveStreamData
+                = env.addSource(new FlinkKafkaConsumer011<String>(
+                                        "sensor",
+                                        new SimpleStringSchema(),
+                                        properties
+                                    )
+                                );
+
+        //transformation-基于流数据进行转换计算
+        DataStream<SensorReading> dataStream = receiveStreamData
+                .map(new MyRichMap());
+
+        //sink-输出结果
+        // 输出到单机redis
+        FlinkJedisPoolConfig config = new FlinkJedisPoolConfig.Builder()
+                .setHost("192.168.10.133")
+                .setPort(6370)
+                .setPassword("123456")
+                .build();
+
+        dataStream.addSink(new RedisSink<>(config,new MyRedisMapper()));
+
+        //执行
+        env.execute();
+
+    }
+
+
+    public static class MyRedisMapper implements RedisMapper<SensorReading> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET,"sensorTmp");
+        }
+
+        @Override
+        public String getKeyFromData(SensorReading sensorReading) {
+            return sensorReading.getId();
+        }
+
+        @Override
+        public String getValueFromData(SensorReading sensorReading) {
+            return sensorReading.getTemperature().toString();
+        }
+    }
+
+    public static class MyRichMap extends RichMapFunction<String,SensorReading> {
+
+        @Override
+        public SensorReading map(String value) throws Exception {
+            String[] valueArr = value.split(",");
+            return new SensorReading(valueArr[0],
+                    Long.valueOf(valueArr[1]),
+                    Double.valueOf(valueArr[2]));
+        }
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            //初始化工作，比如建立数据库的连接，初始化状态等
+            System.out.println(getRuntimeContext().getIndexOfThisSubtask()+":open~~~~~");
+        }
+
+        @Override
+        public void close() throws Exception {
+            //关闭工作，比如关闭数据库连接，清空状态等
+            System.out.println(getRuntimeContext().getIndexOfThisSubtask()+":close!!!");
+        }
+    }
+
+}
